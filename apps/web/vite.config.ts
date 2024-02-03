0ed835cf6f67ce3b1227b185207d537de13719f1
effuone <albkfil@gmail.com>
2024-02-03 10:27:37 +0600

    deploy config added

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,16 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3000,
+    strictPort: true,
+   },
+   server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:3000",
+   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
---
+++ scripts/deploy-api.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+red=$'\e[1;31m'
+green=$'\e[1;32m'
+yellow=$'\e[1;33m'
+blue=$'\e[1;34m'
+magenta=$'\e[1;35m'
+cyan=$'\e[1;36m'
+cls=$'\e[0m'
+
+APP_NAME=hackathon-killer
+PROJECT_DIR="/home/deployer/projects/$APP_NAME"
+APP_DIR=$PROJECT_DIR/apps/api
+
+printf "%b\n" "${yellow}Changing dir to: $APP_DIR ${cls}"
+cd $APP_DIR
+
+if [[ -n $(git status -s) ]]; then
+    printf "%b\n" "${blue}Working directory is not clean, restoring changes... ${cls}"
+    git restore .
+fi
+
+printf "%b\n" "${green}Updating sources... ${cls}"
+git pull origin main
+
+printf "%b\n" "${cyan}Building and redeploying the API container with Docker Compose... ${cls}"
+docker-compose up -d --force-recreate --no-deps api
+
+echo 'Done!'
